f210128c72e0d3635aab2edfa1012e51c30fc71e
ritam28315 <ritam28315@gmail.com>
2022-07-04 21:08:05 +0530

    merge sort visualizer
    04-07-2022

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Merge Sort Visualizer by Ritam Chakraborty</h1>
+      <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
+}
+
+export default App;
